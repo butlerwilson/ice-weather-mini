dc1c03efe3a0ddd003978382f3a33bbe831296e2
ARCTURUS <1638330246@qq>
2022-03-19 00:35:03 +0800

    修复模块地址引入错误

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,6 +1,6 @@
 import qqMap from './utils/qqMap/qqMap'; // 腾讯地图
 import qweather from './utils/weather/qweather'; // 和风天气
-import { getNavBarInfo } from '/utils/navInfo';
+import { getNavBarInfo } from './utils/navInfo';
 import './utils/time';
 
 App({
@@ -14,12 +14,23 @@ App({
     }
 
     // 获取导航栏和状态栏高度
-    const { navHeight, statusBarHeight, capsuleLeft } = getNavBarInfo();
+    const { navHeight, statusBarHeight, capsuleLeft, platform } =
+      getNavBarInfo();
+
+    // 是否是 PC 端
+    let isPC;
+    if (platform !== 'windows' && platform !== 'mac') {
+      isPC = false;
+    } else {
+      isPC = true;
+    }
 
     this.globalData = {
       statusBarHeight,
       capsuleLeft,
       navHeight,
+      platform,
+      isPC,
       qweather,
       qqMap,
     };
